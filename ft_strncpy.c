084143213a58b857bfded7ae49b59110c23bf710
x.r.methula@gmail.com <ray>
2019-05-19 12:08:46 +0200

    ft_strncpy

---
+++ ft_strncpy.c
@@ -0,0 +1,17 @@
+//42 Heading
+
+#include "libft.h"
+
+char	*ft_strncpy(char *dest, const char *src, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while ((src[i] != '\0') && (i < n))
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
