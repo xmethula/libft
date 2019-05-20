220711c4e1fdf579800c0fb551f4894f31b15e70
Xolani METHULA <xmethula@c4r11s1.wethinkcode.co.za>
2019-05-20 15:47:44 +0200

    header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 07:42:23 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/20 08:17:36 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/20 11:46:45 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,18 @@
 #define LIBFT_H
 #include <string.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 size_t		ft_strlen(const char *s);
 void		ft_putchar(char c);
 void		ft_putstr(char *str);
 char		*ft_strcpy(char *dest, const char *src);
 char		*ft_strncpy(char *dest, const char *src, size_t n);
-int		ft_strcmp(const char *s1, const char *s2);
-int		ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_strcmp(const char *s1, const char *s2);
+int			ft_strncmp(const char *s1, const char *s2, size_t n);
+int			ft_atoi(const char *str);
+char		*ft_strdup(const char *s1);
+char		*ft_strcat(char *s1, const char *s2);
+char		*ft_strncat(char *s1, const char *s2, size_t n);
 
 #endif
