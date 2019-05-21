2f47e7fbdee168316c3f3d1782aac1f8f9de760a
Xolani METHULA <xmethula@c4r11s1.wethinkcode.co.za>
2019-05-21 11:40:43 +0200

    ft_strdup file

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 09:31:10 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/20 10:18:57 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/21 10:26:41 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 
 char	*ft_strdup(const char *s1)
 {
-	int i;
-	int len;
-	char *dest;
+	int		i;
+	int		len;
+	char	*dest;
 
 	i = 0;
 	len = 0;
@@ -24,5 +24,12 @@ char	*ft_strdup(const char *s1)
 	{
 		len++;
 	}
-	dest = (char*)str[i]
+	dest = (char*)malloc(sizeof(*dest) * (len + 1));
+	while (i < len)
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
