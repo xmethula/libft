c03e0725ae5775af779a27c8af97aeff91c0b354
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-24 07:21:53 +0200

    header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 07:42:23 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/23 08:38:50 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/24 07:21:33 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,13 @@ size_t		ft_strlen(const char *s);
 int			ft_strcmp(const char *s1, const char *s2);
 int			ft_strncmp(const char *s1, const char *s2, size_t n);
 int			ft_atoi(const char *str);
+int			ft_isupper(int c);
+int			ft_islower(int c);
+int			ft_isalpha(int c);
+int			ft_isdigit(int c);
+int			ft_isalnum(int c);
+int			ft_isascii(int c);
+int			ft_isprint(int c);
 
 char		*ft_strcpy(char *dest, const char *src);
 char		*ft_strncpy(char *dest, const char *src, size_t n);
