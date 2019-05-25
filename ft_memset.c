78973ddd4a4abf61b54f853de50921a6b4c810d6
x.r.methula@gmail.com <ray>
2019-05-26 00:53:18 +0200

    ft_memset fiel

---
+++ ft_memset.c
@@ -0,0 +1,18 @@
+/*vim 42 header*/
+
+#include "libft.h"
+
+void	*ft_memset(void *s, int c, size_t n)
+{
+	char *ptr;
+	size_t i;
+
+	ptr = s;
+	i = 0;
+	while (i < n)
+	{
+		ptr[i] = c;
+		i++;
+	}
+	return (ptr);
+}
