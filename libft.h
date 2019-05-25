f9c29196bede9e9945f72dd54002df9b90020c72
x.r.methula@gmail.com <ray>
2019-05-26 01:43:47 +0200

    header file

--- libft.h
+++ libft.h
@@ -22,7 +22,6 @@ void		ft_putnbr(int n);
 void		ft_putstr(char const *s);
 void		ft_strclr(char *s);
 void		ft_putendl(char const *s);
-void		*ft_memset(void *s, int c, size_t n);
 
 size_t		ft_strlen(const char *s);
 int			ft_strcmp(const char *s1, const char *s2);
@@ -45,5 +44,6 @@ char		*ft_strncpy(char *dest, const char *src, size_t n);
 char		*ft_strdup(const char *s1);
 char		*ft_strcat(char *s1, const char *s2);
 char		*ft_strncat(char *s1, const char *s2, size_t n);
+void		*ft_memset(void *s, int c, size_t n);
 
 #endif
