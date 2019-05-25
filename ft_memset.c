142e50433739d3fc6786da0c67461fcfbbf927f3
x.r.methula@gmail.com <ray>
2019-05-26 01:39:53 +0200

    ft_memset file

--- ft_memset.c
+++ ft_memset.c
@@ -4,15 +4,13 @@
 
 void	*ft_memset(void *s, int c, size_t n)
 {
-	char *ptr;
 	size_t i;
 
-	ptr = s;
 	i = 0;
 	while (i < n)
 	{
-		ptr[i] = c;
+		*(char *)(s + i) = c;
 		i++;
 	}
-	return (ptr);
+	return (s);
 }
