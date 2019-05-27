e9de143622036f3ad09d17b45890543fa9590ecc
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-27 13:14:10 +0200

    ft_memset file

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 08:18:53 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/27 08:19:26 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/27 13:12:45 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	*ft_memset(void *s, int c, size_t n)
 	i = 0;
 	while (i < n)
 	{
-		*(char *)(s + i) = c;
+		((char *)s)[i] = c;
 		i++;
 	}
 	return (s);
