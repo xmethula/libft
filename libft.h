19f3b49f3716f042992600605138f611427ef55d
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-28 12:44:03 +0200

    header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 07:42:23 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/27 10:35:47 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/28 11:10:24 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,12 +39,14 @@ int			ft_toupper(int c);
 int			ft_tolower(int c);
 int			ft_strequ(char const *s1, char const *s2);
 int			ft_strnequ(char const *s1, char const *s2, size_t n);
+int			ft_memcmp(const void *s1, const void *s2, size_t n);
 
 char		*ft_strcpy(char *dest, const char *src);
 char		*ft_strncpy(char *dest, const char *src, size_t n);
 char		*ft_strdup(const char *s1);
 char		*ft_strcat(char *s1, const char *s2);
 char		*ft_strncat(char *s1, const char *s2, size_t n);
+char		*ft_strchr(const char *s, int c);
 void		*ft_memset(void *s, int c, size_t n);
 void		*ft_memcpy(void *dest, void *src, size_t n);
 void		*ft_memccpy(void *dest, void *src, int c, size_t n);
