a2d4ec6fec760929c2338e5b464dbe38ff6f1925
x.r.methula@gmail.com <ray>
2019-05-28 19:24:20 +0200

    ft_strstr file

--- ft_strstr.c
+++ ft_strstr.c
@@ -15,23 +15,25 @@
 char	*ft_strstr(const char *haystack, const char *needle)
 {
 	int i;
+	int j;
 
 	i = 0;
-	if (*needle == '\0')
+	if (needle[i] == '\0')
 	{
 		return ((char *)haystack);
 	}
-	while (*needle != '\0')
+	while (haystack[i] != '\0')
 	{
-		while (haystack[i] == needle[i] && needle[i] != '\0')
+		j = 0;
+		while (needle[j] == haystack[i + j])
 		{
-			i++;
+			if (needle[j + 1] == '\0')
+			{
+				return (&((char *)haystack)[i]);
+			}
+			j++;
 		}
-		if (needle[i] == '\0')
-		{
-			return ((char *)haystack);
-		}
-		haystack++;
+		i++;
 	}
-	return (NULL);
+	return (0);
 }
