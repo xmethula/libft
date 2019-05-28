09688cfdffe40b37a276616435362da47db292cb
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-28 15:46:40 +0200

    header file

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 07:42:23 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/28 11:10:24 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/28 12:45:48 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,6 +47,7 @@ char		*ft_strdup(const char *s1);
 char		*ft_strcat(char *s1, const char *s2);
 char		*ft_strncat(char *s1, const char *s2, size_t n);
 char		*ft_strchr(const char *s, int c);
+char		*ft_strstr(const char *haystack, const char *needle);
 void		*ft_memset(void *s, int c, size_t n);
 void		*ft_memcpy(void *dest, void *src, size_t n);
 void		*ft_memccpy(void *dest, void *src, int c, size_t n);
