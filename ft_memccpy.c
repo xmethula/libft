f57c87dc461ec288b846ba44b80f6acbd533d268
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-30 11:21:26 +0200

    ft_memccpy file

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,13 +6,13 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:36:05 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/27 14:32:26 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/30 10:55:21 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memccpy(void *dest, void *src, int c, size_t n)
+void	*ft_memccpy(void *dst, void *src, int c, size_t n)
 {
 	size_t i;
 
@@ -21,12 +21,12 @@ void	*ft_memccpy(void *dest, void *src, int c, size_t n)
 	{
 		if (((unsigned char *)src)[i] == ((unsigned char)c))
 		{
-			((char *)dest)[i] = ((unsigned char *)src)[i];
-			return (dest);
+			((char *)dst)[i] = ((unsigned char *)src)[i];
+			return (dst);
 		}
-		((char *)dest)[i] = ((unsigned char *)src)[i];
+		((char *)dst)[i] = ((unsigned char *)src)[i];
 		i++;
 	}
-	((char *)dest)[i] = '\0';
-	return (dest);
+	((char *)dst)[i] = '\0';
+	return (dst);
 }
