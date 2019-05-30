136540b6a52d134ce4e6b83f9ae3ce7f59974b4b
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-30 12:27:50 +0200

    ft_memccpy file

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 10:36:05 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/30 10:55:21 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/30 12:27:10 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,14 @@ void	*ft_memccpy(void *dst, void *src, int c, size_t n)
 	size_t i;
 
 	i = 0;
-	while ((((char *)src)[i] != '\0') && (i < n))
+	while (i < n)
 	{
+		((char *)dst)[i] = ((char *)src)[i];	
 		if (((unsigned char *)src)[i] == ((unsigned char)c))
 		{
-			((char *)dst)[i] = ((unsigned char *)src)[i];
-			return (dst);
-		}
-		((char *)dst)[i] = ((unsigned char *)src)[i];
+			return ((char *)dst + i + 1);
+		}	
 		i++;
 	}
-	((char *)dst)[i] = '\0';
-	return (dst);
+	return (NULL);
 }
