5999b47bdcffa13b28e18e62511328b5a7ad7c82
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-30 11:42:06 +0200

    ft_strncpy file

--- ft_strncpy.c
+++ ft_strncpy.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 07:41:56 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/30 10:54:04 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/30 11:41:07 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,10 @@ char	*ft_strncpy(char *dst, const char *src, size_t n)
 		dst[i] = src[i];
 		i++;
 	}
-	dst[i] = '\0';
+	while (i < n)
+	{
+		dst[i] = '\0';
+		i++;
+	}
 	return (dst);
 }
