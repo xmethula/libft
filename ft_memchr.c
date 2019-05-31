695e83208664d274876886bcb507702743f6cfd3
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-31 15:16:32 +0200

    ft_memchr file

--- ft_memchr.c
+++ ft_memchr.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/31 07:55:50 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/31 08:06:02 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/31 15:15:25 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	size_t i;
 
 	i = 0;
-	while ((((unsigned char *)s)[i] != '\0') && ( i < n))
+	while ((((unsigned char *)s)[i] != '\0') && (i < n))
 	{
 		if (((unsigned char *)s)[i] == ((unsigned char)c))
 		{
