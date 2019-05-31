a03b8b1de60fd91b4bb2a9a44860b7546653743a
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-31 12:19:37 +0200

    ft_memcmp file

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/28 08:35:35 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/28 08:50:50 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/05/31 12:16:23 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,16 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n)
 	size_t i;
 
 	i = 0;
-	while ((((char *)s1)[i] == ((char *)s2)[i]) && (i < n))
+	while (i < n)
 	{
-		i++;
+		if (((char *)s1)[i] == ((char *)s2)[i])
+		{
+			i++;
+		}
+		else
+		{
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+		}
 	}
-	return (((char *)s1)[i] - ((char *)s2)[i]);
+	return (0);
 }
