e8582f65ebbbdf43cc181277ab7978ac862b0264
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-05-31 08:11:21 +0200

    ft_memchr file

---
+++ ft_memchr.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 07:55:50 by xmethula          #+#    #+#             */
+/*   Updated: 2019/05/31 08:06:02 by xmethula         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	size_t i;
+
+	i = 0;
+	while ((((unsigned char *)s)[i] != '\0') && ( i < n))
+	{
+		if (((unsigned char *)s)[i] == ((unsigned char)c))
+		{
+			return ((char *)&s[i]);
+		}
+		i++;
+	}
+	if (c == '\0')
+	{
+		return ((char *)&s[i]);
+	}
+	return (NULL);
+}
