98435c2a19d84ca98ccb7c08e30a2251cccf0027
x.r.methula@gmail.com <ray>
2019-06-01 14:04:01 +0200

    ft_memalloc file

---
+++ ft_memalloc.c
@@ -0,0 +1,16 @@
+/**vim 42 header**/
+
+#include "libft.h"
+
+void	*ft_memalloc(size_t size)
+{
+	void *ptr;
+
+	ptr = malloc(size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	ft_bzero(ptr, size);
+	return (ptr);
+}
