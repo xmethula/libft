9d58c3cfdbc32c2b5d397c1d2b26e31cfa21c701
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-06-03 11:12:11 +0200

    ft_memmove file

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 09:49:18 by xmethula          #+#    #+#             */
-/*   Updated: 2019/06/03 09:55:55 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/03 11:10:55 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,10 +16,6 @@ void	*ft_memmove(void *dst, void *src, size_t len)
 {
 	char tmp[len];
 
-	if (tmp == NULL)
-	{
-		return (NULL);
-	}
 	ft_memcpy(tmp, src, len);
 	ft_memcpy(dst, tmp, len);
 	return (dst);
