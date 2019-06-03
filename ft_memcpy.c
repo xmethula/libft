e0baec98632c27a1ff9d92e8c209c4377bc33095
Xolani METHULA <xmethula@c4r12s1.wethinkcode.co.za>
2019-06-03 12:01:54 +0200

    ft_memcpy file

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/27 09:31:40 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/30 12:08:22 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/03 11:59:06 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,20 @@
 
 void	*ft_memcpy(void *dst, void *src, size_t n)
 {
-	size_t i;
+	char	*dptr;
+	char	*sptr;
+	size_t	i;
 
+	dptr = (char *)dst;
+	sptr = (char *)src;
 	i = 0;
+	if ((n == 0) || (dptr == sptr))
+	{
+		return (dst);
+	}
 	while ((i < n))
 	{
-		((char *)dst)[i] = ((char *)src)[i];
+		dptr[i] = sptr[i];
 		i++;
 	}
 	return (dst);
