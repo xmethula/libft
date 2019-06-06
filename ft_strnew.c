b526da7505d1c8a62efbd07fa892147329b578b8
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-06-06 14:29:54 +0200

    ft_strnew file

---
+++ ft_strnew.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 14:04:13 by xmethula          #+#    #+#             */
+/*   Updated: 2019/06/06 14:06:47 by xmethula         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strnew(size_t size)
+{
+	char *str;
+
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	ft_bzero(str, (size + 1));
+	return (str);
+}
