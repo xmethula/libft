b66dd2cec5d2a3d299a84a69fa8062c72f5dfa3e
x.r.methula@gmail.com <ray>
2019-06-08 00:06:43 +0200

    header file

--- libft.h
+++ libft.h
@@ -58,6 +58,7 @@ char		*ft_strstr(const char *haystack, const char *needle);
 char		*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char		*ft_strnew(size_t size);
 char		*ft_strjoin(char const *s1, char const *s2);
+char		*ft_strtrim(char const *s);
 void		*ft_memset(void *b, int c, size_t len);
 void		*ft_memcpy(void *dst, void *src, size_t n);
 void		*ft_memccpy(void *dst, void *src, int c, size_t n);
