c22355852c8eb09fb405ca65f9802641562e4306
x.r.methula@gmail.com <ray>
2019-06-08 20:18:01 +0200

    ft_putnbr file

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -14,7 +14,9 @@
 
 void	ft_putnbr(int n)
 {
-	if (n < 0)
+	if (n == -2147483648)
+		ft_putstr("-2147483648");
+	else if (n < 0)
 	{
 		ft_putchar('-');
 		ft_putnbr(-n);
@@ -25,7 +27,5 @@ void	ft_putnbr(int n)
 		ft_putnbr(n % 10);
 	}
 	else
-	{
 		ft_putchar(n + '0');
-	}
 }
