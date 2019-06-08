b63ac0966bb4ab84ad1ec87d2b4b55b76909805c
x.r.methula@gmail.com <ray>
2019-06-08 19:42:57 +0200

    ft_strmap file

---
+++ ft_strmap.c
@@ -0,0 +1,21 @@
+/** vim 42 header **/
+
+#include "libft.h"
+
+char		*ft_strmap(char const *s, char (*f)(char))
+{
+	size_t	i;
+	char	*str;
+
+	i = 0;
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s)) + 1);
+	if (str == NULL)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
