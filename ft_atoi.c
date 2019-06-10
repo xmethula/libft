513824dcd1cb15c6201636f9b4d2ba90e21d8faa
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-06-10 13:11:53 +0200

    ft_atoi file

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 08:27:56 by xmethula          #+#    #+#             */
-/*   Updated: 2019/06/10 09:32:08 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/10 13:02:53 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ static	void	ft_check(const char *str, int *i)
 
 int				ft_atoi(const char *str)
 {
-	int	i;
-	int	sign;
-	int	res;
+	int			i;
+	int			sign;
+	long long	res;
 
 	i = 0;
 	res = 0;
@@ -40,6 +40,10 @@ int				ft_atoi(const char *str)
 	while ((str[i] != '\0') && (str[i] >= '0' && str[i] <= '9'))
 	{
 		res = res * 10 + (str[i] - '0');
+		if ((res > 2147483648) && (sign == -1))
+			return (0);
+		else if ((res > 2147483647) && (sign == 1))
+			return (-1);
 		i++;
 	}
 	return (sign * res);
