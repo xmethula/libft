fb82cab82a3426c1d2246b7773318850cd093704
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-06-12 15:51:02 +0200

    ft_itoa file

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:51:28 by xmethula          #+#    #+#             */
-/*   Updated: 2019/06/10 09:53:10 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/12 14:56:37 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,8 @@ char		*ft_itoa(int n)
 {
 	char	*str;
 
-	if (!(str = (char *)malloc(sizeof(char) * 2)))
+	str = (char *)malloc(sizeof(char) * 2);
+	if (str == NULL)
 		return (NULL);
 	if (n == -2147483648)
 		return (ft_strcpy(str, "-2147483648"));
@@ -26,9 +27,9 @@ char		*ft_itoa(int n)
 		str[1] = '\0';
 		str = ft_strjoin(str, ft_itoa(-n));
 	}
-	else if (n >= 10)
+	else if (n > 9)
 		str = ft_strjoin(ft_itoa(n / 10), ft_itoa(n % 10));
-	else if (n < 10 && n >= 0)
+	else if ((n >= 0) && (n <= 9))
 	{
 		str[0] = n + '0';
 		str[1] = '\0';
