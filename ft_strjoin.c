554ef8384a14959d0348bc2c6c52ee0fa75cf90f
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-06-13 09:29:37 +0200

    ft_strjoin file

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 08:02:04 by xmethula          #+#    #+#             */
-/*   Updated: 2019/06/10 08:02:15 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/13 09:29:08 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	i = 0;
 	j = 0;
+	if ((s1 == NULL) || (s2 == NULL))
+		return (NULL);
 	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (str == NULL)
 		return (NULL);
