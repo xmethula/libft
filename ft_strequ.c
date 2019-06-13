55a895197e40db80576febb996519dc8ad4704f1
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-06-13 09:22:57 +0200

    ft_strequ file

--- ft_strequ.c
+++ ft_strequ.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 10:29:48 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/24 10:48:36 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/13 09:21:16 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,10 @@
 
 int		ft_strequ(char const *s1, char const *s2)
 {
-	if (ft_strcmp(s1, s2) == 0)
-	{
-		return (1);
-	}
-	else
-	{
+	if ((s1 == NULL) || (s2 == NULL))
 		return (0);
-	}
+	else if (ft_strcmp(s1, s2) == 0)
+		return (1);
+	else
+		return (0);
 }
