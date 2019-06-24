afef09806ebd57c96a251b9c2801f843462a3593
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-06-24 14:28:26 +0200

    ft_strncmp file

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/20 07:41:27 by xmethula          #+#    #+#             */
-/*   Updated: 2019/05/30 15:59:52 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/24 11:48:16 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,13 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	i = 0;
 	while ((s1[i] != '\0') && (s2[i] != '\0') && (s1[i] == s2[i]) && (i < n))
-	{
 		i++;
-	}
 	if (i == n)
-	{
-		i -= 1;
-		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-	}
+		i = i - 1;
+	if (((unsigned char *)s1)[i] > ((unsigned char *)s2)[i])
+		return (1);
+	else if (((unsigned char *)s1)[i] < ((unsigned char *)s2)[i])
+		return (-1);
 	else
-	{
-		return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
-	}
+		return (0);
 }
