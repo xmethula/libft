3e34e0ed3433af16f260b97b4b0f1b55d0ec82a7
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-06-25 09:03:02 +0200

    ft_atoi file

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 07:47:00 by xmethula          #+#    #+#             */
-/*   Updated: 2019/06/24 08:31:25 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/06/25 09:01:51 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,11 +32,8 @@ int					ft_atoi(const char *str)
 	}
 	while ((str[i] != '\0') && (str[i] >= '0') && (str[i] <= '9'))
 	{
-		res = res * 10 + (str[i++] - '0');
-		if ((res > 2147483648) && (sign == -1))
-			return (0);
-		else if ((res > 2147483647) && (sign == 1))
-			return (-1);
+		res = res * 10 + (str[i] - '0');
+		i++;
 	}
 	return (res * sign);
 }
