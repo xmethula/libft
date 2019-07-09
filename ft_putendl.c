da67fb0d4626b2e97d2adc677aa2f60cbe9ff0b9
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-07-09 11:35:02 +0200

    ft_putendl file

--- ft_putendl.c
+++ ft_putendl.c
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 11:22:16 by xmethula          #+#    #+#             */
-/*   Updated: 2019/06/25 12:58:27 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/07/09 11:34:32 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,6 @@
 
 void	ft_putendl(char const *s)
 {
-	int		i;
-	char	*str;
-
-	i = 0;
-	if (s == NULL)
-	{
-		str = "(null)";
-		while (str[i] != '\0')
-		{
-			ft_putchar(str[i]);
-			i++;
-		}
-	}
-	else
-	{
-		while (s[i] != '\0')
-		{
-			ft_putchar(s[i]);
-			i++;
-		}
-		ft_putchar('\n');
-	}
+	ft_putstr(s);
+	ft_putchar('\n');
 }
