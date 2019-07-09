a7204280cf4b4bc43cce2e0276f7fc35d4c95a81
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-07-09 12:51:54 +0200

    ft_strsplit file

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -6,18 +6,46 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/10 09:56:41 by xmethula          #+#    #+#             */
-/*   Updated: 2019/06/21 12:25:59 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/07/09 12:46:49 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_strsplit(char const *s, char c)
+static int		ft_countword(char const *s, char c)
 {
-	int		i;
-	int		start;
-	int		row;
-	char	**arr;
+	int			i;
+	int			ctr;
+
+	i = 0;
+	ctr = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c)
+			ctr++;
+		i++;
+	}
+	return (ctr);
+}
+
+static char		*ft_strndup(char const *s1, size_t len)
+{
+	char		*str;
+
+	str = (char *)malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	ft_strncpy(str, s1, len);
+	str[len] = '\0';
+	return (str);
+}
+
+char			**ft_strsplit(char const *s, char c)
+{
+	int			i;
+	int			start;
+	int			row;
+	char		**arr;
 
 	i = 0;
 	row = 0;
