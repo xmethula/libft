e6c55a0b346480fd00b9799648d652f5ffd4ec67
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-08-07 10:14:18 +0200

    main.c

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/31 12:08:29 by xmethula          #+#    #+#             */
-/*   Updated: 2019/07/31 15:38:16 by xmethula         ###   ########.fr       */
+/*   Created: 2019/08/06 09:36:57 by xmethula          #+#    #+#             */
+/*   Updated: 2019/08/07 10:13:57 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,9 @@ typedef struct		s_list
 	struct s_list	*next;
 }					t_list;
 
-void	print(t_list *node)
+void	print_lst(t_list *node)
 {
-	while (node != NULL)
+	while (node)
 	{
 		ft_putnbr_endl(node->data);
 		node = node->next;
@@ -32,7 +32,7 @@ int		lst_len(t_list *node)
 	int		len;
 
 	len = 0;
-	while (node != NULL)
+	while (node)
 	{
 		len++;
 		node = node->next;
@@ -40,21 +40,19 @@ int		lst_len(t_list *node)
 	return (len);
 }
 
-void	add_start(t_list **node, int val)
+void	add_begin(t_list **node, int val)
 {
 	t_list		*new;
 
 	new = (t_list *)malloc(sizeof(t_list));
 	new->data = val;
-	if (*node == NULL)
-	{	
-		new->next = NULL;
-		*node = new;
-	}
+	new->next = NULL;
+	if ((*node) == NULL)
+		(*node) = new;
 	else
 	{
-		new->next = *node;
-		*node = new;
+		new->next = (*node);
+		(*node) = new;
 	}
 }
 
@@ -66,39 +64,35 @@ void	add_end(t_list **node, int val)
 	new = (t_list *)malloc(sizeof(t_list));
 	new->data = val;
 	new->next = NULL;
-	if (*node == NULL)
-		*node = new;
+	if ((*node) == NULL)
+		(*node) = new;
 	else
 	{
-		i = *node;
-		while (i->next != NULL)
+		i = (*node);
+		while (i->next)
 			i = i->next;
 		i->next = new;
 	}
 }
 
-void	add_pos(t_list **node, int val, int pos)
+void	add_after(t_list **node, int val, int after)
 {
 	t_list		*new;
 	t_list		*i;
 	int			len;
 	int			j;
 
+	len = lst_len((*node));
 	new = (t_list *)malloc(sizeof(t_list));
 	new->data = val;
-	len = lst_len(*node);
-	if (pos > len)
+	new->next = NULL;
+	if (after > len)
 		ft_putendl("error");
-	else if (pos == 1)
-	{
-		new->next = (*node)->next;
-		(*node)->next = new;
-	}
 	else
 	{
 		j = 1;
-		i = *node;
-		while (j < pos)
+		i = (*node);
+		while (j < after)
 		{
 			i = i->next;
 			j++;
@@ -106,62 +100,69 @@ void	add_pos(t_list **node, int val, int pos)
 		new->next = i->next;
 		i->next = new;
 	}
-
 }
 
-void	del_start(t_list **node)
+void	del_begin(t_list **node)
 {
 	t_list		*tmp;
 
-	tmp = *node;
-	*node = tmp->next;
+	tmp = (*node);
+	(*node) = tmp->next;
 	tmp->next = NULL;
 	free(tmp);
 }
 
 void	del_end(t_list **node)
 {
+	t_list		*p;
 	t_list		*tmp;
-	t_list		*i;
 	int			len;
-	int			j;
+	int			i;
 
-	len = lst_len(*node);
-	j = 1;
-	i = *node;
-	while (j < (len - 1))
-	{
-		i = i->next;
-		j++;
-	}
-	tmp = i->next;
-	i->next = NULL;
-	free(tmp);
-}
-
-void	del_pos(t_list **node, int val, int pos)
-{
-	t_list		*tmp;
-	t_list		*i;
-	int			len;
-	int			j;
-
-	len = lst_len(*node);
-	if (pos > len)
-		ft_putendl("error");
-	else if (pos == 1)
-		del_start(node);
+	len = lst_len((*node));
+	if (len == 1)
+		del_begin(node);
 	else
 	{
-		j = 1;
-		i = *node;
-		while (j < (pos - 1))
+		i = 1;
+		p = (*node);
+		while (i < (len -1))
 		{
-			i = i->next;
-			j++;
+			p = p->next;
+			i++;
 		}
-		tmp = i->next;
+		tmp = p->next;
+		p->next = tmp->next;
+		tmp->next = NULL;
+		free(tmp);
+	}
+}
 
+void	del_at(t_list **node, int at)
+{
+	t_list		*p;
+	t_list		*tmp;
+	int			i;
+	int			len;
+
+	len = lst_len(*node);
+	if (at > len)
+		ft_putendl("error");
+	else if (len == 1)
+		del_begin(node);
+	else
+	{
+		i = 1;
+		p = (*node);
+		while (i < (at - 1))
+		{
+			p = p->next;
+			i++;
+		}
+		tmp = p->next;
+		p->next = tmp->next;
+		tmp->next = NULL;
+		free(tmp);
 	}
 }
 
@@ -170,28 +171,27 @@ int		main(void)
 	t_list		*root;
 
 	root = NULL;
-	add_start(&root, 5);
-	print(root);
+	add_begin(&root, 10);
+	print_lst(root);
 	ft_putchar('\n');
 
-	add_end(&root, 10);
-	print(root);
+	add_begin(&root, 5);
+	print_lst(root);
 	ft_putchar('\n');
 
-	add_end(&root, 20);
-	print(root);
+	add_end(&root, 15);
+	print_lst(root);
 	ft_putchar('\n');
 
-	add_pos(&root, 15, 2);
-	print(root);
+	add_after(&root, 20, 3);
+	print_lst(root);
 	ft_putchar('\n');
 
-	del_start(&root);
-	print(root);
+	del_begin(&root);
+	print_lst(root);
 	ft_putchar('\n');
 
-	del_end(&root);
-	print(root);
-	ft_putchar('\n');
+	del_at(&root, 2);
+	print_lst(root);
 	return (0);
 }
